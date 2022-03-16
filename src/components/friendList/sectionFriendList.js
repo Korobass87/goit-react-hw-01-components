68c1a6e9418779b54d54e3ff986c6b52ec27e2fe
Korobass87 <88404829+Korobass87@users.noreply.github.com>
2022-03-16 21:43:15 +0300

    Delete src/components/friendList directory

--- src/components/friendList/sectionFriendList.js
+++
@@ -1,9 +0,0 @@
-import { Container } from './profile.styled'
-import List from "./friendList"
-import friends from"./friends.json"
-export default function SectionFrienList() {
-    
-    return <Container>
-        <List friendsArr={friends}/>
-    </Container>
-}
